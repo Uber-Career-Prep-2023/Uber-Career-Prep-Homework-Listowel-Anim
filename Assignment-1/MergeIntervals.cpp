95f0abc6412b3fa73bfe2e5a3d1c9d5c3f74de1e
Listowel <animlistowel@gmail.com>
2023-03-14 20:26:46 -0400

    Update MergeIntervals.cpp

--- Assignment-1/MergeIntervals.cpp
+++ Assignment-1/MergeIntervals.cpp
@@ -1,1 +1,2 @@
-
+//I just couldn't find a way to make an array of integer pairs in C++ and also to return an array of integer pair. 
+//I wasn't able to come up with a solution to this problem withing the time limit of 40  minutes.
